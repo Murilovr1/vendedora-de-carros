a06482b9fd06a8e54950e04630d4bdb2f826db7b
Murilo V. R. Garcia <muriloviniciusrodriguesgarcia@gmail.com>
2025-04-29 15:29:00 -0300

    nova atualização

--- core/admin.py
+++ core/admin.py
@@ -7,6 +7,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from core import models
+from core.models.Acessório import Acessorio
 
 
 class UserAdmin(BaseUserAdmin):
